1ed8faa14835a27bd438ea7c92702a302d79ce17
nishimura-shohei <38713140+nishimura-shohei@users.noreply.github.com>
2018-05-09 13:34:36 +0900

    Update w10_main_ex02.js

--- W10/w10_main_ex02.js
+++ W10/w10_main_ex02.js
@@ -12,7 +12,7 @@ function main()
     var bounds = Bounds( volume );
     screen.scene.add( bounds );
 
-    var material.color = new THREE.Color();
+    var material.color = THREE.Color();
     
     screen.scene.add( material );
     
