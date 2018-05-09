3e388a1d8d67ec2915f8afcf14bc98358fd122b4
nishimura-shohei <38713140+nishimura-shohei@users.noreply.github.com>
2018-05-09 13:42:44 +0900

    Add files via upload

--- W10/w10_main_ex02.js
+++ W10/w10_main_ex02.js
@@ -12,10 +12,6 @@ function main()
     var bounds = Bounds( volume );
     screen.scene.add( bounds );
 
-    var material.color = new THREE.Color(0xff0000);
-    
-    screen.scene.add( material );
-    
     var isovalue = 128;
     var surfaces = Isosurfaces( volume, isovalue );
     screen.scene.add( surfaces );
@@ -23,8 +19,6 @@ function main()
     document.addEventListener( 'mousemove', function() {
         screen.light.position.copy( screen.camera.position );
     });
-    
-    
 
     window.addEventListener( 'resize', function() {
         screen.resize( [ window.innerWidth, window.innerHeight ] );
